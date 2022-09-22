0fe2cc2b74b5c0b572183e2319113546134ac299
saartwito <saartwito49@gmail.com>
2022-09-22 14:38:58 +0300

    .

--- src/screens/SubmitSuccessful/SubmitSuccessful.tsx
+++ src/screens/SubmitSuccessful/SubmitSuccessful.tsx
@@ -3,11 +3,10 @@ import logo from 'assets/logo.svg';
 import './submitSuccessful.scss'
 
 
-
 const SubmitSuccessful = () => {
   return (
     <div className="submit-successful">
-      <img src={logo} alt="Golden tape logo" />
+      <img src={logo} alt="Looshes logo" />
       <h1>!הטופס נשלח בהצלחה</h1>
     </div>
   )
