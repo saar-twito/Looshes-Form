f0eafac8cd4d30591eff48ecc039796cc2f73905
saartwito <saartwito49@gmail.com>
2022-12-13 13:57:29 +0200

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,12 @@ const Header = () => {
     <header className='golden-tape-form-header'>
       <img src={logo} alt='' />
       <h1>form for a new client</h1>
+      <small>
+        <strong>
+          THE FORM IS INTENDED FOR PRESENTING A PORTFOLIO AND NOT FOR REAL
+          PUBLIC SERVICE.
+        </strong>
+      </small>
     </header>
   );
 };
